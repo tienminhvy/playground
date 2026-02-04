a0a10aeecab3647ebff3d458311496cbfe48a895
Vy Tien <me@tienminhvy.com>
2026-02-04 07:56:37 +0700

    feat: initialize window wrapper component

---
+++ src/app/components/applications/window-wrapper/window-wrapper.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-window-wrapper',
+  templateUrl: './window-wrapper.html',
+  styleUrl: './window-wrapper.css',
+})
+export class WindowWrapper {
+
+}
